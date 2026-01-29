69e580accc79aad119a09ac1bfb9d8d9d716c590
Anshidtp <shaanshid07@gmail.com>
2026-01-29 12:30:38 +0530

    packages changed

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Anshidtp.github.io/portfolio/', 
+  base: '/my_portfolio/portfolio/', 
   server: {
     port: 3000,
     open: true
